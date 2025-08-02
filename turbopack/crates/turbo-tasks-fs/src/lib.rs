e82981376907cae412637675082c56e400209fdf
Tobias Koppers <tobias.koppers@googlemail.com>
2025-08-02 07:12:08 +0200

    rename methods for clippy

--- turbopack/crates/turbo-tasks-fs/src/lib.rs
+++ turbopack/crates/turbo-tasks-fs/src/lib.rs
@@ -325,7 +325,7 @@ impl DiskFileSystemInner {
             .chain(dir_invalidator_map)
             .flat_map(|(_, invalidators)| invalidators.into_keys())
             .collect::<Vec<_>>();
-        parallel::into_for_each(invalidators, |invalidator| invalidator.invalidate());
+        parallel::vec_into_for_each(invalidators, |invalidator| invalidator.invalidate());
     }
 
     /// Invalidates every tracked file in the filesystem.
@@ -348,7 +348,7 @@ impl DiskFileSystemInner {
                     .map(move |i| (reason_for_path.clone(), i))
             })
             .collect::<Vec<_>>();
-        parallel::into_for_each(invalidators, |(reason, invalidator)| {
+        parallel::vec_into_for_each(invalidators, |(reason, invalidator)| {
             invalidator.invalidate_with_reason(reason)
         });
     }
